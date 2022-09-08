0ca525dab5c5626591ae374293b03e6acd1b9df3
Brian Connolly <brian@thingsoutofstuff.com>
2022-09-08 12:06:36 -0700

    Clean up for documentation

--- Projects/Stoplight/pinTest.js
+++ Projects/Stoplight/pinTest.js
@@ -1,3 +1,4 @@
+// Export button values so they can be watched on the Edit page
 export var buttonZero
 export var buttonOne
 export var buttonTwo
@@ -7,7 +8,7 @@ export var buttonFive
 export var buttonSix
 export var buttonSeven
 
-
+// Assign the pin address for each button
 var BUTTON_ZERO_PIN = 14
 var BUTTON_ONE_PIN = 21
 var BUTTON_TWO_PIN = 22
@@ -17,6 +18,7 @@ var BUTTON_FIVE_PIN = 27
 var BUTTON_SIX_PIN = 33
 var BUTTON_SEVEN_PIN = 36
 
+// Initialize button pins
 pinMode(BUTTON_ZERO_PIN, INPUT_PULLDOWN)
 pinMode(BUTTON_ONE_PIN, INPUT_PULLDOWN)
 pinMode(BUTTON_TWO_PIN, INPUT_PULLDOWN)
@@ -27,6 +29,7 @@ pinMode(BUTTON_SIX_PIN, INPUT_PULLDOWN)
 pinMode(BUTTON_SEVEN_PIN, INPUT_PULLDOWN)
 
 export function beforeRender(delta) {
+  // Read values from each button pin
   buttonZero = digitalRead(BUTTON_ZERO_PIN)
   buttonOne = digitalRead(BUTTON_ONE_PIN)
   buttonTwo = digitalRead(BUTTON_TWO_PIN)
@@ -42,54 +45,45 @@ export function render(index) {
   hsv(.0, 0, 0)
   
   if (index == 0) {
-    
     if (buttonZero > 0) {
-      hsv(.2, .1, 1)  
+      hsv(.2, .1, 1) // LED on for index 0
     }
     
   } else if (index == 1) {
-  
     if (buttonOne > 0) {
-      hsv(.4, .1, 1)  
+      hsv(.4, .1, 1) // LED on for index 1
     }
     
   } else if (index == 2) {
-    
     if (buttonTwo > 0) {
-      hsv(.7, 1, 1)  
+      hsv(.7, 1, 1) // LED on for index 2  
     }
     
   } else if (index == 3) {
-    
     if (buttonThree > 0) {
-      hsv(.55, 1, 1)  
+      hsv(.55, 1, 1) // LED on for index 3  
     }
     
   } else if (index == 4) {
-    
     if (buttonFour > 0) {
-      hsv(.25, 1, 1)  
+      hsv(.25, 1, 1) // LED on for index 4  
     }
     
   } else if (index == 5) {
-    
     if (buttonFive > 0) {
-      hsv(.15, 1, 1)  
+      hsv(.15, 1, 1) // LED on for index 5  
     }
     
   } else if (index == 6) {
-    
     if (buttonSix > 0) {
-      hsv(.85, 1, 1)  
+      hsv(.85, 1, 1) // LED on for index 6  
     }
     
   } else if (index == 7) {
-    
     if (buttonSeven > 0) {
-      hsv(.95, 1, 1)  
+      hsv(.95, 1, 1) // LED on for index 7  
     }
     
-  }
-  
+  } 
 
 }
