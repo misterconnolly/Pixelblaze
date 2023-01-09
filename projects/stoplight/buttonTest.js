9ddb5199c309a4ac60f7386ebec92d40d1a3dd12
Brian Connolly <brian@thingsoutofstuff.com>
2023-01-08 18:54:23 -0800

    Simplify button example

--- projects/stoplight/buttonTest.js
+++ projects/stoplight/buttonTest.js
@@ -1,14 +1,5 @@
-// Export button values so they can be watched on the Edit page
-export var buttonZero
-export var buttonOne
-export var buttonTwo
-export var buttonThree
-export var buttonFour
-export var buttonFive
-export var buttonSix
-export var buttonSeven
+// Use with a strip of at least eight pixels
 
-// Assign the pin address for each button
 var BUTTON_ZERO_PIN = 26
 var BUTTON_ONE_PIN = 25
 var BUTTON_TWO_PIN = 36
@@ -18,7 +9,6 @@ var BUTTON_FIVE_PIN = 22
 var BUTTON_SIX_PIN = 27
 var BUTTON_SEVEN_PIN = 33
 
-// Initialize button pins
 pinMode(BUTTON_ZERO_PIN, INPUT_PULLDOWN)
 pinMode(BUTTON_ONE_PIN, INPUT_PULLDOWN)
 pinMode(BUTTON_TWO_PIN, INPUT_PULLDOWN)
@@ -28,8 +18,16 @@ pinMode(BUTTON_FIVE_PIN, INPUT_PULLDOWN)
 pinMode(BUTTON_SIX_PIN, INPUT_PULLDOWN)
 pinMode(BUTTON_SEVEN_PIN, INPUT_PULLDOWN)
 
+export var buttonZero
+export var buttonOne
+export var buttonTwo
+export var buttonThree
+export var buttonFour
+export var buttonFive
+export var buttonSix
+export var buttonSeven
+
 export function beforeRender(delta) {
-  // Read values from each button pin
   buttonZero = digitalRead(BUTTON_ZERO_PIN)
   buttonOne = digitalRead(BUTTON_ONE_PIN)
   buttonTwo = digitalRead(BUTTON_TWO_PIN)
@@ -45,45 +43,47 @@ export function render(index) {
   hsv(.0, 0, 0)
 
   if (index == 0) {
-    if (buttonZero > 0) {
-      hsv(.2, .1, 1) // LED on for index 0
+    if (buttonZero) {
+      setPixelGreen()
     }
 
   } else if (index == 1) {
-    if (buttonOne > 0) {
-      hsv(.4, .1, 1) // LED on for index 1
+    if (buttonOne) {
+      setPixelGreen()
     }
 
   } else if (index == 2) {
-    if (buttonTwo > 0) {
-      hsv(.7, 1, 1) // LED on for index 2  
+    if (buttonTwo) {
+      setPixelGreen()
     }
 
   } else if (index == 3) {
-    if (buttonThree > 0) {
-      hsv(.55, 1, 1) // LED on for index 3  
+    if (buttonThree) {
+      setPixelGreen() 
     }
 
   } else if (index == 4) {
-    if (buttonFour > 0) {
-      hsv(.25, 1, 1) // LED on for index 4  
+    if (buttonFour) {
+      setPixelGreen()  
     }
 
   } else if (index == 5) {
-    if (buttonFive > 0) {
-      hsv(.15, 1, 1) // LED on for index 5  
+    if (buttonFive) {
+      setPixelGreen()  
     }
 
   } else if (index == 6) {
-    if (buttonSix > 0) {
-      hsv(.85, 1, 1) // LED on for index 6  
+    if (buttonSix) {
+      setPixelGreen()  
     }
 
   } else if (index == 7) {
-    if (buttonSeven > 0) {
-      hsv(.95, 1, 1) // LED on for index 7  
+    if (buttonSeven) {
+      setPixelGreen()  
     }
-
   }
+}
 
+function setPixelGreen() {
+  hsv(.3, 1, 1)
 }
